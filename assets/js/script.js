fa214f1857385e1c88c4fb04bc70de5d7aeb0468
Sarah <sarahsitisumaerah@gmail.com>
2021-07-31 20:40:15 +0700

    upload first version

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+const menuToogle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToogle.addEventListener('click', function() {
+    nav.classList.toggle('slide');
+});
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 5000); // Change image every 3 seconds
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
